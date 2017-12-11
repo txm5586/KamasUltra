6ec456449627c977bf4751e5da4472746fd172ad
Tassio Marques <tassiomm@gmail.com>
2017-12-11 13:15:48 +0100

    Initial Commit

---
+++ KamasUltra/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KamasUltra
+//
+//  Created by Tassio Moreira Marques on 11/12/2017.
+//  Copyright © 2017 Tassio Marques. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
